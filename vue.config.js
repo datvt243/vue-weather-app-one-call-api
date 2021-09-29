f3f74708af000352b3f591959032f8332fa54662
DatVT <votan.it@gmail.com>
2021-09-29 23:08:30 +0700

    first upload

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,15 @@
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === "production" ? "/vue-base-template" : "/",
+    process.env.NODE_ENV === "production"
+      ? "/vue-weather-app-one-call-api"
+      : "/",
   pages: {
     index: {
       entry: "src/main.js",
       template: "public/index.html",
       filename: "index.html",
-      title: "Vuejs - Event list with Firebase",
-      chunks: ["chunk-vendors", "chunk-common", "index"]
+      title: "Vuejs - Weather app one call API",
+      chunks: ["chunk-vendors", "chunk-common", "index"],
     },
   },
 };
